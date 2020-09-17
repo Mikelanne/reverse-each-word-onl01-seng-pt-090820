fdf4773e129a54541185f348172a703ac7436999
Mikelanne Wood <mikelanne.wood@gmail.com>
2020-09-17 17:51:21 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,10 +1,11 @@
 require 'pry'
 
 def reverse_each_word(string)
-  reversed = []
-  split_string = string.split
-  split_string.each do |word|
-    word.reverse
+  binding.pry
+ # reversed = []
+ # split_string = string.split
+ # split_string.each do |word|
+   # puts word.reverse
   end
  end
 end
