2a666a541af5d7bda16f57e7cdcf55b29fa28e34
Mikelanne Wood <mikelanne.wood@gmail.com>
2020-09-17 17:56:27 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -8,3 +8,10 @@ def reverse_each_word(string)
   end
   reversed_words.join(" ")
 end
+
+def reverse_each_word(string)
+ split_string = string.split
+ split_string.collect do |word|
+  word.reverse.join
+  end
+end
