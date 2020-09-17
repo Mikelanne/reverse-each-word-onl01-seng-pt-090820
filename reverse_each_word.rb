5666567787d40480a5156108aa0da07042428bc1
Mikelanne Wood <mikelanne.wood@gmail.com>
2020-09-17 17:58:23 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,10 +1,17 @@
 require 'pry'
 
+#def reverse_each_word(string)
+ #reversed_words = []
+# split_string = string.split
+# split_string.each do |word|
+#  reversed_words << (word.reverse)
+#  end
+#  reversed_words.join(" ")
+#end
+
 def reverse_each_word(string)
- reversed_words = []
- split_string = string.split
- split_string.each do |word|
-  reversed_words << (word.reverse)
+  split_string = string.split
+  split_string.each do |word|
+  word.reverse
   end
-  reversed_words.join(" ")
 end
